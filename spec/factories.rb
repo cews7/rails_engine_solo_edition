41cde60f28c6b76f9c19f2aa947965c81aa57df4
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2017-02-14 14:22:50 -0700

    write model test for invoiceitem and customer

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,5 +1,13 @@
 FactoryGirl.define do
-  factory :model do
-
+  factory :invoice_item do
+    item_id 1
+    invoice_id 1
+    quantity 1
+    unit_price 1
+  end
+  
+  factory :customer do
+    first_name Faker::Name.first_name
+    last_name Faker::Name.last_name
   end
 end
