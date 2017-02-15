5d40653a8bb4e7a7498f02847cb83a3d99c89782
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2017-02-15 13:42:33 -0700

    write test for returning an invoice

--- spec/requests/api/v1/invoice/request_spec.rb
+++ spec/requests/api/v1/invoice/request_spec.rb
@@ -15,4 +15,18 @@ RSpec.describe Invoice, type: :request do
     expect(invoices.first).to have_key(:customer_id)
     expect(invoices.first).to have_key(:merchant_id)
   end
+
+  it 'returns invoice' do
+    db_invoice = create(:invoice)
+
+    get "/api/v1/invoices/#{db_invoice.id}"
+
+    expect(response).to be_success
+
+    invoice_attrs = JSON.parse(response.body, symbolize_names: true)
+
+    expect(invoice_attrs.count).to eq 6
+    expect(invoice_attrs).to have_key(:customer_id)
+    expect(invoice_attrs).to have_key(:merchant_id) 
+  end
 end
