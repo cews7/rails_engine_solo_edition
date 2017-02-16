96c0c122681d09f11a7e53dc63e23132c955fb66
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2017-02-16 15:18:23 -0700

    add tests for multiple types of invoice_item lookups, add transactions find all route and search controller along with invoice_items find_all route and search controller.

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,13 @@ Rails.application.routes.draw do
       end
 
       namespace :transactions do
-        get '/find', to: 'search#show'
+        get '/find',     to: 'search#show'
+        get '/find_all', to: 'search#index'
+      end
+
+      namespace :invoice_items do
+        get '/find',     to: 'search#show'
+        get '/find_all', to: 'search#index'
       end
 
       resources :merchants, only: [:index, :show]
