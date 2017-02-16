853435f1e153491270b2ea73b9f796b47173a835
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2017-02-16 10:31:58 -0700

    add date-time with precision to items table

---
+++ db/migrate/20170216173039_add_date_time_with_precision_to_items.rb
@@ -0,0 +1,6 @@
+class AddDateTimeWithPrecisionToItems < ActiveRecord::Migration[5.0]
+  def change
+    add_column :items, :created_at, :datetime, precision: 0
+    add_column :items, :updated_at, :datetime, precision: 0
+  end
+end
