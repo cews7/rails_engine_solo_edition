33b0014f89de5523e969137493023c691787b881
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2017-02-21 11:04:17 -0700

    update failing tests

---
+++ spec/factories/merchants.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do 
+  factory :merchant do
+    name Faker::LordOfTheRings.character
+  end
+end
--- spec/requests/api/v1/invoices/request_spec.rb
+++ spec/requests/api/v1/invoices/request_spec.rb
@@ -19,8 +19,9 @@ RSpec.describe Invoice, type: :request do
 
   it 'returns all invoices -- customer_id lookup' do
     merchant       = create(:merchant)
-    invoice_clone1 = Invoice.create(id: 1, customer_id: 1, merchant_id: merchant.id, status: "success")
-    invoice_clone2 = Invoice.create(id: 2, customer_id: 1, merchant_id: merchant.id, status: "success")
+    customer       = create(:customer, id: 1)
+    invoice_clone1 = Invoice.create(id: 1, customer_id: customer.id, merchant_id: merchant.id, status: "success")
+    invoice_clone2 = Invoice.create(id: 2, customer_id: customer.id, merchant_id: merchant.id, status: "success")
     invoice_uniq   = Invoice.create(id: 3, customer_id: 2, merchant_id: merchant.id, status: "success")
 
     get '/api/v1/invoices/find_all?customer_id=1'
@@ -37,9 +38,10 @@ RSpec.describe Invoice, type: :request do
   it 'returns all invoices -- merchant_id lookup' do
     merchant       = create(:merchant, id: 1)
     merchant2      = create(:merchant)
-    invoice_clone1 = Invoice.create(customer_id: 1, merchant_id: merchant.id, status: "success")
-    invoice_clone2 = Invoice.create(customer_id: 1, merchant_id: merchant.id, status: "success")
-    invoice_uniq   = Invoice.create(customer_id: 2, merchant_id: merchant2.id, status: "success")
+    customer       = create(:customer)
+    invoice_clone1 = Invoice.create(customer_id: customer.id, merchant_id: merchant.id, status: "success")
+    invoice_clone2 = Invoice.create(customer_id: customer.id, merchant_id: merchant.id, status: "success")
+    invoice_uniq   = Invoice.create(customer_id: customer.id, merchant_id: merchant2.id, status: "success")
 
     get '/api/v1/invoices/find_all?merchant_id=1'
 
@@ -54,9 +56,10 @@ RSpec.describe Invoice, type: :request do
 
   it 'returns all invoices -- status lookup' do
     merchant       = create(:merchant)
-    invoice_clone1 = Invoice.create(customer_id: 1, merchant_id: merchant.id, status: "failed")
-    invoice_clone2 = Invoice.create(customer_id: 1, merchant_id: merchant.id, status: "success")
-    invoice_uniq   = Invoice.create(customer_id: 2, merchant_id: merchant.id, status: "success")
+    customer       = create(:customer)
+    invoice_clone1 = Invoice.create(customer_id: customer.id, merchant_id: merchant.id, status: "failed")
+    invoice_clone2 = Invoice.create(customer_id: customer.id, merchant_id: merchant.id, status: "success")
+    invoice_uniq   = Invoice.create(customer_id: customer.id, merchant_id: merchant.id, status: "success")
 
     get '/api/v1/invoices/find_all?status=success'
 
