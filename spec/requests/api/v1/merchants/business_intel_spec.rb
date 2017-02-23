f4d9bd93ca69f7f2361ecc50c48a73906b4e723b
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2017-02-23 10:39:39 -0700

    write passing test for returning top merchants based on items sold

--- spec/requests/api/v1/merchants/business_intel_spec.rb
+++ spec/requests/api/v1/merchants/business_intel_spec.rb
@@ -38,4 +38,42 @@ RSpec.describe 'Merchant Business Intelligence' do
     expect(top_merchants.first).to have_value('John')
     expect(top_merchants.last).to have_value('Tim')
   end
+
+  it 'by default, returns top 2 merchants based on number of items sold' do
+    merchant_1 = create(:merchant)
+    merchant_2 = create(:merchant, name: "Tim")
+    merchant_3 = create(:merchant, name: "John")
+
+    merchant_1.invoices << create_list(:invoice, 2)
+    merchant_2.invoices << create_list(:invoice, 2)
+    merchant_3.invoices << create_list(:invoice, 2)
+
+    merchant_1.invoices.each do |invoice|
+      invoice.transactions  << create(:transaction)
+      invoice.invoice_items << create(:invoice_item, quantity: 10)
+      invoice.invoice_items << create(:invoice_item, quantity: 20)
+    end
+
+    merchant_2.invoices.each do |invoice|
+      invoice.transactions  << create(:transaction)
+      invoice.invoice_items << create(:invoice_item, quantity: 30)
+      invoice.invoice_items << create(:invoice_item, quantity: 40)
+    end
+
+    merchant_3.invoices.each do |invoice|
+      invoice.transactions  << create(:transaction)
+      invoice.invoice_items << create(:invoice_item, quantity: 50)
+      invoice.invoice_items << create(:invoice_item, quantity: 60)
+    end
+
+    get '/api/v1/merchants/most_items?quantity=2'
+
+    expect(response).to be_success
+
+    top_merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(top_merchants.count).to eq 2
+    expect(top_merchants.first).to have_value('John')
+    expect(top_merchants.last).to have_value('Tim')
+  end
 end
