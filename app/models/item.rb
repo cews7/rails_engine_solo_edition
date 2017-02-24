274b16fdd6c8a5c632fc5c0a091edc847001811d
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2017-02-24 10:10:34 -0700

    write passing test for returning top item(s) by revenue

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,13 @@ class Item < ApplicationRecord
 
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+
+  def self.most_revenue(quantity)
+    select('items.*')
+    .joins(invoices: [:invoice_items, :transactions])
+    .merge(Transaction.successful)
+    .group(:id)
+    .order('sum(invoice_items.quantity * invoice_items.unit_price) desc')
+    .take(quantity)
+  end
 end
--- spec/requests/api/v1/items/business_intel_spec.rb
+++ spec/requests/api/v1/items/business_intel_spec.rb
@@ -1,7 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe 'Items Business Intelligence' do
-  it 'returns top x items ranked by total revenue' do
+  it 'returns top x (1 as default) items ranked by total revenue' do
+    item     = create(:item)
+    top_item = create(:item, name: 'Pencil')
 
+    item.invoice_items << create_list(:invoice_item, 2, unit_price: 1, quantity: 1)
+
+    top_item.invoice_items << create_list(:invoice_item, 2, unit_price: 2, quantity: 2)
+
+    item.invoices.first.transactions << create(:transaction, result: 'success')
+    top_item.invoices.first.transactions << create(:transaction, result: 'success')
+
+    get "/api/v1/items/most_revenue?quantity=1"
+
+    expect(response).to be_success
+
+    most_rev_item = JSON.parse(response.body, symbolize_names: true)
+
+    expect(most_rev_item.first).to have_value('Pencil')
   end
 end
